d9e0cf83e1baed2d1b657bf39742fb4823569a8d
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-10 16:08:10 +0000

    first commit

---
+++ src/main/java/com/codeclan/example/Whisky_Distillery/WhiskyDistilleryApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Whisky_Distillery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyDistilleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyDistilleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/Whisky_Distillery/WhiskyDistilleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Whisky_Distillery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyDistilleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
